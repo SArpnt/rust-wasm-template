1d7e12c9b1fd5b1407b01785a6b9bdfa9e7f58b1
SArpnt <36542932+SArpnt@users.noreply.github.com>
2021-12-01 00:21:49 -0500

    add missing shebang

--- rust source/build.sh
+++ rust source/build.sh
@@ -1,3 +1,4 @@
+#!/bin/sh
 cargo build --target wasm32-unknown-unknown --release
 wasm-bindgen target/wasm32-unknown-unknown/debug/*.wasm --out-dir ../web_build --out-name rustWasm --target web --no-typescript
 wasm-opt ../web_build/rustWasm_bg.wasm -Os -o ../web_build/rustWasm_bg.wasm
